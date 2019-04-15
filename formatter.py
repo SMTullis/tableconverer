3263c8cf29d64cd347151c40f92172a7de8a7171
Sean M Tullis <stullis@fs.fed.us>
2019-04-15 12:54:33 -0600

    Add TableFormatter.data_table
    TableFormatter.data_table is a Table to store the data found by the fomatter.

--- formatter.py
+++ formatter.py
@@ -1,8 +1,10 @@
 import re
+import table
 
 class TableFormatter:
-    def __init__(self, **regexes):
+    def __init__(self, pay_data, **regexes):
         self.reg = regexes
+        self.data_table = table.Table(pay_data)
 
     def CheckRegex(self, regex, line):
         return self.reg[regex].findall(line)
