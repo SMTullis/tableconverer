29d2077e478a867ab75150a5d9fdfa69dc1102e9
Sean M Tullis <stullis@fs.fed.us>
2019-04-15 13:00:50 -0600

    Add Find methods to TableFormatter

--- formatter.py
+++ formatter.py
@@ -8,3 +8,21 @@ class TableFormatter:
 
     def CheckRegex(self, regex, line):
         return self.reg[regex].findall(line)
+
+    def FindUSDANo(self, line):
+        return self.CheckRegex("USDA", line)
+
+    def FindDODNo(self, line):
+        self.CheckRegex("DOD", line)
+
+    def FindEffectiveDate(self, line):
+        return self.CheckRegex("date", line)
+
+    def FindState (self):
+        return self.CheckRegex("state", line)
+
+    def FindGrade(self, line):
+        return self.CheckRegex("grade", line)
+
+    def FindPayRates(self, line):
+        return self.CheckRegex("rates", line)
