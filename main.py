35d470953354dcdb51f4551a1f68d833a7c9af40
Sean M Tullis <stullis@fs.fed.us>
2019-04-18 07:37:21 -0600

    Add a dash to the state regex.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     regexes = {
         "USDA": re.compile("USDA# (\d{4})"),
         "DOD": re.compile("AC-(\d{4})"),
-        "state": re.compile("for the [\w\s]+?, (\w+)"),
+        "state": re.compile("for the [\w\s-]+?, (\w+)"),
         "date": re.compile("Effective Date: ([\w ]+)"),
         "grade": re.compile("(?=.*\d{1,3}\.\d{2}(?:[]\s\*]+)\d{1,3}\.\d{2})^.+?(?:WS-)?(\d{1,2})"),
         "rates": re.compile("(\d{1,3}\.\d{2})")
